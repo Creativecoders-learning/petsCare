df5b3d5d8ccdbc573f67baad70897386aa1941fc
iftakher99 <19tonmoy99@gmail.com>
2024-10-25 17:33:35 +0600

    primary and secondary color added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#675BC8', // Replace with your primary color hex code
+        secondary: '#0A453A', // Replace with your secondary color hex code
+      },
+    },
   },
   plugins: [],
-}
-
+};
