65890d0668adf1544e4a2f1af39d280c145f1a41
msetu1 <msetu5763@gmail.com>
2024-10-25 22:36:15 +0600

    code add

--- frontend/src/Pages/Vets/Vets.jsx
+++ frontend/src/Pages/Vets/Vets.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 
 const Vets = () => {
+    const [value, setValue]=useState(false);
+
     return (
         <div>
             this is a vets page
@@ -10,6 +13,9 @@ const Vets = () => {
                 </div>
                 <div>
                     this is hasan
+                </div>
+                <div>
+                   error tw deyar kotha
                 </div>
             </div>
         </div>
