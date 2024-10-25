0a51cc244ca6aa4e012cc47c080be913f7c26f1c
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-10-25 10:27:20 +0600

    test remote branch

---
+++ frontend/src/Components/UI/Button.jsx
@@ -0,0 +1,6 @@
+
+export default function Button({children}) {
+  return (
+    <div>{children}</div>
+  )
+}
---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Button from "../../Components/UI/Button";
+
+export default function Home() {
+  return (
+    <div>
+        <Section1/>
+        
+        <Button></Button>
+    </div>
+  )
+}
