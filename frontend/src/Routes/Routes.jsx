e5ffeefa8c262716a1adc9b2dfa8177c1aa21dd5
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-10-26 19:09:49 +0600

    Fixed Conflict

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../ErrorPage";
-import Home from "../pages/Home/Home";
 import Adoption from "../Pages/Adoption/Adoption";
 import Shop from "../Pages/Shop/Shop";
 const router = createBrowserRouter([
@@ -11,7 +10,6 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <Home /> },
       { path: "adoption", element: <Adoption /> },
       { path: "shop", element: <Shop /> },
     ],
