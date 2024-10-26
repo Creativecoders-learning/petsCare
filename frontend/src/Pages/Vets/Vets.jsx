8d39ba2ce5df167673090f63880954bfbcf1a192
msetu1 <msetu5763@gmail.com>
2024-10-26 21:27:07 +0600

    code

--- frontend/src/Pages/Vets/Vets.jsx
+++ frontend/src/Pages/Vets/Vets.jsx
@@ -3,7 +3,6 @@ import VetsBanner from "../../Components/VetsBanner/VetsBanner";
 import VetsCard from "../../Components/VetsCard/VetsCard";
 
 const Vets = () => {
-<<<<<<< HEAD
   const [vets, setVets] = useState();
   useEffect(() => {
     fetch("/vetsFakeData.json")
@@ -24,27 +23,6 @@ const Vets = () => {
       </div>
     </div>
   );
-=======
-    const [value, setValue]=useState(false);
-
-    return (
-        <div>
-            this is a vets page
-            <div>
-                Hi i am a | No this is no hi 
-                <div>
-                    this is hasan
-                </div>
-                <div>
-                    this is hasan
-                </div>
-                <div>
-                   error tw deyar kotha
-                </div>
-            </div>
-        </div>
-    );
->>>>>>> 4c5fd01410425e8a95cf65d046c2375d0b4523e3
 };
 
 export default Vets;
