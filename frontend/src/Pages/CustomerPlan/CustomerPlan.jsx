d630f492ff3a77305512223e0b297ac30cdc5f95
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-10-26 12:37:07 +0600

    opened new branch Bayezid

---
+++ frontend/src/Pages/CustomerPlan/CustomerPlan.jsx
@@ -0,0 +1,10 @@
+
+const CustomerPlan = () => {
+      return (
+            <div>
+                  THis is customer plan page
+            </div>
+      );
+};
+
+export default CustomerPlan;
--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -2,13 +2,17 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import ErrorPage from '../ErrorPage';
 import Home from '../pages/Home/Home';
+import CustomerPlan from '../Pages/CustomerPlan/CustomerPlan';
 const router = createBrowserRouter([
   // this is basic routes
   {
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
-    children: [{ path: '/', element: <Home /> }],
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/customer-plan', element: <CustomerPlan /> },
+    ],
   },
 ]);
 
