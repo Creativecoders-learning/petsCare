84f983ecb802b38ad7064ccf7b690e2de9e79889
Sayem-hussain-majhi <hafejmdsayem@gmail.com>
2024-10-27 20:30:59 +0600

    blog details page added

--- frontend/src/Pages/Blogs/BlogDetails.jsx
+++ frontend/src/Pages/Blogs/BlogDetails.jsx
@@ -1,14 +1,24 @@
 import { useParams } from 'react-router-dom'
-import img from '../../assets/dog3.jpg';
+// import img from '../../assets/dog3.jpg';
 import Container from '../../Components/UI/Container';
 import useBlogs from '../../Hooks/api/useBlogs';
+import { useEffect, useState } from 'react';
 
 const BlogDetails = () => {
     const {loading, error, blogs} = useBlogs(); 
+    const [items, setItems] = useState({})
     const {id} = useParams()
 
-    console.log(blogs)
+    const blog =  blogs.find(blog => blog.id === Number(id))
+
+    console.log( blog, items)
+    useEffect(()=>{
+        const subBlog = blog?.subtitles
+        setItems(subBlog)
+    },[blog])
+
     if (loading) return <p>Loading...</p>;
+    if (!blog) return <div>Blog not found</div>;
     if (error) return <p>Error: {error}</p>;
     
     return (
@@ -18,18 +28,28 @@ const BlogDetails = () => {
             <div className='w-full h-[60vh] overflow-hidden m-0 p-0'>
                 <img className='w-full h-full object-cover  '
                 // style={{ clipPath: 'inset(5% 0 10% 0)' }}
-                src={img} alt="" />
+                src={blog?.image} alt="" />
             </div>
             <article className='text-center mt-5 mb-3'>
-                <p className='text-gray-600'>PET BLOG / <span className='font-semibold text-gray-950'>Dog</span></p>
-                <h1 className="text-2xl font-semibold">Make Barktastic Things With Pumpkin For Fall</h1>
-                <p className='text-sm text-gray-900'>October 5, 2024</p>
+                <p className='text-gray-600'>PET BLOG / <span className='font-semibold text-gray-950'>{blog?.category}</span></p>
+                <h1 className="text-3xl text-gray-900 font-bold">{blog?.title}</h1>
+                
             </article>
 
             <article>
-                <p className='text-sm text-gray-800'>Want to boost your cat or dog's diet with something nutritious? Pumpkin for pets can be a game changer, offering a rich source of nutrition and fiber to promote regular digestion. It's a simple yet effective way to enhance their diet—whether you have a playful pup or a curious cat, adding pumpkin can support their digestive health and add a tasty twist to their daily routine.</p>
+                <p className='text-sm text-gray-800 my-5'>{blog?.description}</p>
             </article>
-            Blog Details
+             {/* subtitle */}
+             <div className='mt-10'>
+               {
+                    items?.map(item => 
+                        <article key={item?.title}>
+                            <h1 className='text-2xl font-bold text-gray-800 mt-10 mb-3'>{item?.title} hello</h1>
+                            <p className='text-gray-700 mb-5'>{item?.content}</p>
+                        </article>
+                    )
+                }
+               </div>
         </div>
         </Container>
     );
