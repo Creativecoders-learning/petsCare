cd9d8915cd8864ee1ed36aa1e93488d2d144d631
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-11-01 22:51:04 +0600

    added vet management modal

---
+++ frontend/src/Image-data/Vet/patients.js
@@ -0,0 +1,7 @@
+import bella from '../../assets/Patients/bella.jpg'
+import buddy from '../../assets/Patients/buddy.jpg'
+import whiskers from '../../assets/Patients/whiskers.jpg'
+
+export const PAIENTSImages = {
+      bella, buddy, whiskers
+}
