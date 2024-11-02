9f559a29a7725e78891e2364e35ab7edc905b609
Umme-Nahida <mysuccessmission@gmail.com>
2024-11-02 22:27:14 +0600

    redesign home page

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,9 +2,11 @@ import Puppies from '../../Components/Adoption/Puppies/Puppies';
 import About from '../../Components/Home/About/About';
 import AdoptionSteps from '../../Components/Home/AdoptionSteps/AdoptionSteps';
 import Banner from '../../Components/Home/Banner/Banner';
+import NewsLetter from '../../Components/Home/NewsLetter/NewsLetter';
 import PetNews from '../../Components/Home/PetNews/PetNews';
 import Products from '../../Components/Home/Products/Products';
 import Stats from '../../Components/Home/Stats/Stats';
+import Testimonials from '../../Components/Home/Testimonials/Testimonials';
 import Container from '../../Components/UI/Container';
 
 const Home = () => {
@@ -18,6 +20,8 @@ const Home = () => {
       <AdoptionSteps />
       <Products />
       <PetNews />
+      <Testimonials></Testimonials>
+      <NewsLetter/>
     </Container>
     </div>
   );
