b60ade974df7ebbb87aa8af8bd91193f781e74e6
Umme-Nahida <mysuccessmission@gmail.com>
2024-11-07 00:01:51 +0600

    create all adoption api

--- backend/Modules/Adoption/AdoptionAPI.js
+++ backend/Modules/Adoption/AdoptionAPI.js
@@ -1,12 +1,39 @@
 const express = require('express')
+
+function adoptionAPI(adoptionCollection){
 const adoptionRouter = express.Router();
 
 
+// add adoption 
+adoptionRouter.post('/addAdoption',async(req,res)=>{
+    const adoptionInfo = req.body;
+    const result = await adoptionCollection.insertOne(adoptionInfo)
+    res.send(result)
+})
+
+// get all adoption and get my adoption data
 adoptionRouter.get('/getAdoption',async(req,res)=>{
-    const adoptionCollection = req.app.locals.adoptionCollection;
-    const result = await adoptionCollection.find().toArray()
+    const email = req.query.email;
+    const query = {};
+    if(email){
+        query = {email: email}
+    }
+    const result = await adoptionCollection.find(query).toArray()
      res.send(result)
 })
 
+}
 
-module.exports=adoptionRouter
+function ReceiverAdoption(recieverList){
+    const recieverAdoption = express.Router()
+    // get reciever adoption data 
+    recieverAdoption.get('/userAdoption',async(req,res)=>{
+    const email = req.query.email;
+    const query = {receiver_email:email}
+    const result = await adoptionCollection.find(query).toArray()
+    res.send(result)
+})
+}
+
+
+module.exports={ReceiverAdoption,adoptionAPI}
