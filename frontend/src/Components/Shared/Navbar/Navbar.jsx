b42a4e810a040a87620eb47e3f3b69a58c211705
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-11-26 00:05:46 +0600

    Fixed Navbar responsive

--- frontend/src/Components/Shared/Navbar/Navbar.jsx
+++ frontend/src/Components/Shared/Navbar/Navbar.jsx
@@ -41,39 +41,96 @@ export default function Navbar() {
       });
   };
 
+  // navLinks
+  const navLinks = (
+    <>
+      <li className="flex items-center">
+        <ActiveRoute to={"/"}>
+          <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
+            Home
+          </span>
+        </ActiveRoute>
+      </li>
+      <li className="flex items-center">
+        <ActiveRoute to={"/shop"}>
+          <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
+            Shop
+          </span>
+        </ActiveRoute>
+      </li>
+      <li className="flex items-center">
+        <ActiveRoute to={"vets"}>
+          <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
+            Vets
+          </span>
+        </ActiveRoute>
+      </li>
+      <li className="flex items-center">
+        <ActiveRoute to={"/adoption"}>
+          <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
+            Adoption
+          </span>
+        </ActiveRoute>
+      </li>
+      <li className="flex items-center">
+        <ActiveRoute to={"blogs"}>
+          <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
+            Blogs
+          </span>
+        </ActiveRoute>
+      </li>
+      <li className="flex items-center">
+        <ActiveRoute to={"customer-plan"}>
+          <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
+            Customer Plans
+          </span>
+        </ActiveRoute>
+      </li>
+      <div className="lg:hidden flex gap-4 items-center">
+        <Link to="/login">
+          <Button primary>Login</Button>
+        </Link>
+        <Link to="/registration">
+          <Button secondary> Sign Up</Button>
+        </Link>
+      </div>
+    </>
+  );
+
   return (
     <>
       <header className="relative z-20 w-full after:absolute  after:left-0 after:top-full after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:backdrop-blur-sm lg:after:hidden pt-[16px] pb-[8px]">
         <Container>
-          <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem] z-50">
-            <nav
-              aria-label="main navigation"
-              className="flex items-center justify-between font-medium text-slate-700"
-              role="navigation"
-            >
-              {/*      <!-- Brand logo --> */}
-              <div className="">
-                <Link
-                  to="/"
-                  aria-label="WindUI logo"
-                  aria-current="page"
-                  className="flex items-center gap-2 whitespace-nowrap text-2xl focus:outline-none lg:flex-1 text-primary"
+          <div className="relative mx-auto max-w-full z-50 lg:px-6 px-2">
+            <nav className="flex items-center gap-6 justify-between font-medium text-slate-700">
+              <div className="w-full flex justify-between items-center">
+                {/*      <!-- Brand logo --> */}
+                <div className="order-2 lg:order-1">
+                  <Link to="/" className="text-primary text-3xl">
+                    PetsCare
+                  </Link>
+                </div>
+                
+                {/*      <!-- Navigation links small device --> */}
+                <ul className="order-2 hidden lg:flex justify-between items-center gap-2">
+                  {navLinks}
+                </ul>
+                <div
+                  className="relative p-4 cursor-pointer hover:scale-[1.1] transition-all duration-300 ease-in-out order-1 lg:order-3"
+                  onClick={handleShopCart}
                 >
-                  PetsCare
-                </Link>
+                  <div className="absolute top-0 right-0 px-2 py-1  rounded-full text-xs bg-red-400 text-white">
+                    {cartStatus}
+                  </div>
+                  <FaShoppingCart className="text-2xl" />
+                </div>
+                <div className="lg:hidden flex order-4"></div>
               </div>
+
               {/*      <!-- Mobile trigger --> */}
               <button
-                className={`relative order-10 block h-10 w-10 self-center lg:hidden
-                ${
-                  isToggleOpen
-                    ? "visible opacity-100 [&_span:nth-child(1)]:w-6 [&_span:nth-child(1)]:translate-y-0 [&_span:nth-child(1)]:rotate-45 [&_span:nth-child(2)]:-rotate-45 [&_span:nth-child(3)]:w-0 "
-                    : ""
-                }
-              `}
+                className="relative order-10 block h-10 w-10 self-center lg:hidden"
                 onClick={() => setIsToggleOpen(!isToggleOpen)}
-                aria-expanded={isToggleOpen ? "true" : "false"}
-                aria-label="Toggle navigation"
               >
                 <div className="absolute left-1/2 top-1/2 w-6 -translate-x-1/2 -translate-y-1/2 transform">
                   <span
@@ -90,78 +147,8 @@ export default function Navbar() {
                   ></span>
                 </div>
               </button>
-              {/*      <!-- Navigation links --> */}
-              <ul
-                role="menubar"
-                aria-label="Select page"
-                className={`absolute left-0 top-0 z-[-1] w-full justify-center overflow-hidden  overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-24 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0  lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0  lg:pt-0 lg:opacity-100 ${
-                  isToggleOpen
-                    ? "visible opacity-100 backdrop-blur-sm"
-                    : "invisible opacity-0"
-                }`}
-              >
-                <li className="flex items-center">
-                  <ActiveRoute to={"/"}>
-                    <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
-                      Home
-                    </span>
-                  </ActiveRoute>
-                </li>
-                <li className="flex items-center">
-                  <ActiveRoute to={"/shop"}>
-                    <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
-                      Shop
-                    </span>
-                  </ActiveRoute>
-                </li>
-                <li className="flex items-center">
-                  <ActiveRoute to={"vets"}>
-                    <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
-                      Vets
-                    </span>
-                  </ActiveRoute>
-                </li>
-                <li className="flex items-center">
-                  <ActiveRoute to={"/adoption"}>
-                    <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
-                      Adoption
-                    </span>
-                  </ActiveRoute>
-                </li>
-                <li className="flex items-center">
-                  <ActiveRoute to={"blogs"}>
-                    <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
-                      Blogs
-                    </span>
-                  </ActiveRoute>
-                </li>
-                <li className="flex items-center">
-                  <ActiveRoute to={"customer-plan"}>
-                    <span className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-primary lg:px-4">
-                      Customer Plans
-                    </span>
-                  </ActiveRoute>
-                </li>
-                <div className="lg:hidden flex gap-4 items-center">
-                  <Link to="/login">
-                    <Button primary>Login</Button>
-                  </Link>
-                  <Link to="/registration">
-                    <Button secondary> Sign Up</Button>
-                  </Link>
-                </div>
-              </ul>
               {/*      <!-- Actions --> */}
               <div className="flex items-center gap-10">
-                <div
-                  onClick={handleShopCart}
-                  className="relative p-4 cursor-pointer hover:scale-[1.1] transition-all duration-300 ease-in-out"
-                >
-                  <div className="absolute top-0 right-0 px-2 py-1  rounded-full text-xs bg-red-400 text-white">
-                    {cartStatus}
-                  </div>
-                  <FaShoppingCart className="text-2xl" />
-                </div>
                 {!user ? (
                   <>
                     <div className="hidden lg:flex gap-4 items-center">
@@ -247,6 +234,18 @@ export default function Navbar() {
             </nav>
           </div>
         </Container>
+        {/*      <!-- Navigation links small device --> */}
+        <ul
+          role="menubar"
+          aria-label="Select page"
+          className={`absolute left-0 top-0 z-[-1] w-full justify-center overflow-hidden  overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-24 font-medium transition-[opacity,visibility] duration-300 ${
+            isToggleOpen
+              ? "visible opacity-100 backdrop-blur-sm"
+              : "invisible opacity-0"
+          }`}
+        >
+          {navLinks}
+        </ul>
         <div>
           <img
             className="w-full absolute top-[80%]"
